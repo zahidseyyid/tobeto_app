5a245bb5537df39f049cd7fff40e35809f3afa9e
MuhammetMM <sevenciltepe2004@gmail.com>
2023-12-25 14:05:10 +0300

    gradiant son

--- lib/widgets/home_page/gradientcard_widget.dart
+++ lib/widgets/home_page/gradientcard_widget.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/home_page/startbutton_widget.dart';
 
 class GradientCardWidget extends StatelessWidget {
-  const GradientCardWidget({Key? key});
+  final String buttonText;
+  final LinearGradient gradient;
+
+  const GradientCardWidget({
+    Key? key,
+    required this.buttonText,
+    required this.gradient,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,32 +17,22 @@ class GradientCardWidget extends StatelessWidget {
       elevation: 4,
       child: Container(
         height: 150,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.only(
             topRight: Radius.circular(30),
             bottomLeft: Radius.circular(30),
             bottomRight: Radius.circular(30),
           ),
-          gradient: LinearGradient(
-            begin: Alignment(0.5, -1.0),
-            end: Alignment(0.5, 1.0),
-            colors: [
-              Color(0xFF1D0B8C),
-              Color(0xFFBDA6FE),
-            ],
-            stops: [0.1357, 0.9838],
-            transform: GradientRotation(209.21 * 3.14159 / 180.0),
-          ),
+          gradient: gradient,
         ),
         child: Column(
           children: [
-            const Padding(
-              padding:
-                  EdgeInsets.only(top: 40), // Metni alta indirmek için padding
+            Padding(
+              padding: const EdgeInsets.only(top: 40),
               child: Center(
                 child: Text(
-                  'Profilini oluştur',
-                  style: TextStyle(
+                  buttonText,
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 24,
                     fontWeight: FontWeight.w800,
