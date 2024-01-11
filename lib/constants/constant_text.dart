08160961682a2785b18b4d5236fed7fee36d0955
Hamza Zahid <zahidseyyid@gmail.com>
2024-01-11 18:33:18 +0300

    fix

--- lib/constants/constant_text.dart
+++ lib/constants/constant_text.dart
@@ -1,3 +1,6 @@
+
+// Homepage Constants
+
 const nameUser = 'Halit Enes';
 const dialogTitle = 'Herkes için Kodlama 1D Değerlendirme Sınavı';
 const dialogText = " Sevgili Adayımız, \n\nHerkes için Kodlama Eğitimi'ni tamamladığınız için tebrik ederiz. Bu eğitim sonrası bir sonraki aşamaya geçiş için 25 sorudan oluşan bir değerlendirme sınavımız bulunmaktadır. \n\nBu test her kullanıcı için sadece 1 kez sunulmakta olup 45 dakika içinde tamamlanması gerekmektedir. \n\nTüm değerlendirme kriterleri sonrası Mesleki Gelişim Eğitimlerine geçişiniz ile ilgili bilgilendirileceksiniz. \n\nSevgiler, \nTOBETO";
@@ -5,3 +8,7 @@ const correctAnswer = "24";
 const wrongAnswer = "1";
 const emptyAnswer = "0";
 const point = "96";
+
+// Login Page Constants
+
+
