6e7f4a41db4a282fd12435cdb85411fa5f972f4e
beyzakoser <b.koser@hotmail.com>
2024-01-11 17:18:31 +0300

    surveys widget added

---
+++ lib/widgets/home_page/tabbar_widgets/surveys_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/constant_image.dart';
+
+class SurveysWidget extends StatelessWidget {
+  const SurveysWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 5),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Color(0xFFe6e6e6), //değişecek
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Column(
+          children: [
+            Image.asset(surveyImage,
+                height: MediaQuery.of(context).size.height / 6),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, bottom: 10),
+              child: Text(
+                "Atanmış herhangi bir anketiniz bulunmamaktadır",
+                style: TextStyle(
+                    fontSize: 14,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.secondary),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
