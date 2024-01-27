dce16f73952a4ce202b4b8747d261eecb8a89462
Hamza Zahid <zahidseyyid@gmail.com>
2024-01-27 09:29:14 +0300

    fix

--- lib/widgets/education_details_page/education_about.dart
+++ lib/widgets/education_details_page/education_about.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/constant_padding.dart';
+
 import 'package:flutter_application_1/datas/education_dummy_data.dart';
 import 'package:flutter_application_1/widgets/education_details_page/education_details_about_widget.dart';
 
@@ -15,40 +16,66 @@ class _EducationAboutState extends State<EducationAbout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children: [
-          EducationDetailsAboutWidget(
-              title: "Başlangıç:",
-              icon: Icons.calendar_month,
-              description:
-                  "${dummyEducations[0].startDate.day}/0${dummyEducations[0].startDate.weekday}/${dummyEducations[0].startDate.year}"),
-          EducationDetailsAboutWidget(
-              title: "Bitiş:",
-              icon: Icons.calendar_month,
-              description:
-                  "${dummyEducations[0].endDate.day}/0${dummyEducations[0].endDate.weekday}/${dummyEducations[0].endDate.year}"),
-          Padding(padding: paddingSmall),
-          EducationDetailsAboutWidget(
-              title: "Geçirdiğin Süre:",
-              icon: Icons.watch_later_outlined,
-              description: "${dummyEducations[0].spentTime.inHours} saat"
-                  " ${dummyEducations[0].spentTime.inMinutes.remainder(60)} dakika"),
-          Padding(padding: paddingSmall),
-          EducationDetailsAboutWidget(
-              title: "Tahmini Süre:",
-              icon: Icons.watch_later_outlined,
-              description: "${dummyEducations[0].estimatedTime.inHours} saat"
-                  " ${dummyEducations[0].estimatedTime.inMinutes.remainder(60)} dakika"),
-          Padding(padding: paddingSmall),
-          EducationDetailsAboutWidget(
-              title: "Kategori:", icon: Icons.category, description: dummyEducations[0].category),
-          Padding(padding: paddingSmall),
-          EducationDetailsAboutWidget(title: "Eğitimci:", icon: Icons.person, description: dummyEducations[0].teacher),
-          Padding(padding: paddingSmall),
-          EducationDetailsAboutWidget(
-              title: "Video Sayısı:", icon: Icons.language, description: dummyEducations[0].videoCount.toString()),
-        ],
-      )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+          child: ListView.builder(
+            itemCount: 8, // Öğe sayısı
+            padding: paddingMedium,
+            itemBuilder: (context, index) {
+              switch (index) {
+                case 0:
+                  return EducationDetailsAboutWidget(
+                    title: "Başlangıç:",
+                    icon: Icons.calendar_month,
+                    description:
+                        "${dummyEducations[0].startDate.day}/0${dummyEducations[0].startDate.weekday}/${dummyEducations[0].startDate.year}",
+                  );
+                case 1:
+                  return EducationDetailsAboutWidget(
+                    title: "Bitiş:",
+                    icon: Icons.calendar_month,
+                    description:
+                        "${dummyEducations[0].endDate.day}/0${dummyEducations[0].endDate.weekday}/${dummyEducations[0].endDate.year}",
+                  );
+                case 2:
+                  return EducationDetailsAboutWidget(
+                    title: "Geçirdiğin Süre:",
+                    icon: Icons.watch_later_outlined,
+                    description: "${dummyEducations[0].spentTime.inHours} saat"
+                        " ${dummyEducations[0].spentTime.inMinutes.remainder(60)} dakika",
+                  );
+                case 3:
+                  return EducationDetailsAboutWidget(
+                    title: "Tahmini Süre:",
+                    icon: Icons.watch_later_outlined,
+                    description: "${dummyEducations[0].estimatedTime.inHours} saat"
+                        " ${dummyEducations[0].estimatedTime.inMinutes.remainder(60)} dakika",
+                  );
+                case 4:
+                  return EducationDetailsAboutWidget(
+                    title: "Kategori:",
+                    icon: Icons.category,
+                    description: dummyEducations[0].category,
+                  );
+                case 5:
+                  return EducationDetailsAboutWidget(
+                    title: "Eğitimci:",
+                    icon: Icons.person,
+                    description: dummyEducations[0].teacher,
+                  );
+                case 6:
+                  return EducationDetailsAboutWidget(
+                    title: "Video Sayısı:",
+                    icon: Icons.language,
+                    description: dummyEducations[0].videoCount.toString(),
+                  );
+                default:
+                  return const SizedBox.shrink(); // Fazladan indeksler için boş bir widget döndürür
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
