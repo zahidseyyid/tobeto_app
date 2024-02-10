848834bfebcefb658a5de0ecfd7d99128d8f82ad
beyzakoser <b.koser@hotmail.com>
2024-02-11 02:09:56 +0300

    update announcement_models

--- lib/models/announcements_model.dart
+++ lib/models/announcements_model.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AnnouncementModel {
   final String id;
   final String title;
@@ -13,4 +15,41 @@ class AnnouncementModel {
       required this.isAnnouncement,
       required this.isRead,
       required this.date});
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'id': id});
+    result.addAll({'title': title});
+    result.addAll({'text': text});
+    result.addAll({'isAnnouncement': isAnnouncement});
+    result.addAll({'isRead': isRead});
+    result.addAll({'date': date.millisecondsSinceEpoch});
+
+    return result;
+  }
+
+  factory AnnouncementModel.fromMap(Map<String, dynamic> map) {
+    return AnnouncementModel(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      text: map['text'] ?? '',
+      isAnnouncement: map['isAnnouncement'] ?? false,
+      isRead: map['isRead'] ?? false,
+      date: (map['date'] as Timestamp).toDate(),
+    );
+  }
+
+  factory AnnouncementModel.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot) {
+    final map = snapshot.data() as Map;
+    return AnnouncementModel(
+      id: map['id'] ?? '',
+      title: map['title'] ?? '',
+      text: map['text'] ?? '',
+      isAnnouncement: map['isAnnouncement'] ?? false,
+      isRead: map['isRead'] ?? false,
+      date: (map['date'] as Timestamp).toDate(),
+    );
+  }
 }
